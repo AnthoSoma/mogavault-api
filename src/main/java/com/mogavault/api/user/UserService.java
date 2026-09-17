f009d1e30f9d281fb7d62111fc02c4532a6dabc4
Arkanos <anthony.pierron54@gmail.com>
2026-09-17 10:43:11 +0200

    feat(login): wip User feature

---
+++ src/main/java/com/mogavault/api/user/UserService.java
@@ -0,0 +1,31 @@
+package com.mogavault.api.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserProfileResponse getUserById(UUID id) {
+        return userRepository.findById(id)
+                .map(UserProfileResponse::fromEntity)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cannot find user with id: " + id));
+    }
+
+    public UserProfileResponse getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .map(UserProfileResponse::fromEntity)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cannot find user with username: " + username));
+    }
+}
