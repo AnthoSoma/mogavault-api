c377233287dbb1a683a45f564a6ab90b5914d777
Arkanos <anthony.pierron54@gmail.com>
2026-09-21 21:34:31 +0200

    feat(login): wip user

--- src/main/java/com/mogavault/api/config/GlobalExceptionHandler.java
+++ src/main/java/com/mogavault/api/config/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.mogavault.api.config;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
 import java.time.Instant;
@@ -13,16 +16,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ProblemDetail handleValidationExceptions(MethodArgumentNotValidException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.BAD_REQUEST,
-                "Data validation failed"
-        );
-
-        problemDetail.setTitle("Invalid data");
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        ProblemDetail problemDetail = ex.getBody();
+        problemDetail.setTitle("Invalid Request");
+        problemDetail.setDetail("Data validation has failed");
         problemDetail.setType(URI.create("https://mogavault.dev/errors/validation-error"));
         problemDetail.setProperty("timestamp", Instant.now());
 
@@ -33,6 +38,6 @@ public class GlobalExceptionHandler {
 
         problemDetail.setProperty("errors", invalidFields);
 
-        return problemDetail;
+        return ResponseEntity.status(status).body(problemDetail);
     }
 }
